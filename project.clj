47f9180006db073ee2ab4e58550c6ead31244b31
Dan Midwood <dan@danmidwood.com>
2014-01-13 14:02:15 -0500

    Rename group-id from com.diffbot to just diffbot, and add clojars release settings to project

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
-(defproject com.diffbot/diffbot "0.1.0-SNAPSHOT"
+(defproject diffbot/diffbot "0.1.0-SNAPSHOT"
   :description "Clojure interface to the Diffbot API"
   :url "http://www.diffbot.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.8"]]
+  :lein-release {:scm :git
+                 :deploy-via :clojars}
   :profiles {:dev {:source-paths ["src-dev"]
                    :dependencies [[midje "1.6.0"]
                                   [clj-http-fake "0.7.8"]
